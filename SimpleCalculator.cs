180e2920487a1e57b693d66ea2894d1efc5b24ba
Adnan Wattoo <fakewattoo092@gmail.com>
2025-03-25 11:35:52 -0700

    Add files via upload

---
+++ SimpleCalculator.cs
@@ -0,0 +1,35 @@
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
